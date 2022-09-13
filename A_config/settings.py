83e8b426cb9d251eea892a0f54cad9ae204c235b
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2022-09-13 17:12:50 +0500

    asdasdasdsa

--- A_config/settings.py
+++ A_config/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'A_config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'EducationDemoBase',
+        'PASSWORD': '1',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
+
 
 
 # Password validation
